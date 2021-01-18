caaa98afb2478b7d2af308b37af45e9482cd9f9b
kapilkumar2001 <kapilkumar2001prajapat@gmail.com>
2021-01-18 09:19:46 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
